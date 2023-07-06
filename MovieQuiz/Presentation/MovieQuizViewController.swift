5b1a703ceb5c76d412efcfb3e51f3f100ce022f2
Alex <133873788+DonMerkulio@users.noreply.github.com>
2023-07-06 23:42:47 +0300

    Сделана начальная верстка кнопок, и лейбла с текстом "вопрос"

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,10 +1,16 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+    
+    
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    
 }
 
 /*
